80b2a65c1ba737cadba51b28e9c04d00b99b0977
Nadia <enadialopez@gmail.com>
2022-07-25 12:37:10 -0300

    Rotten Tomatoes

---
+++ README.md
@@ -0,0 +1,3 @@
+Rotten Tomatoes
+
+Es una app de opiniones y clasificaciones de peliculas.
---
+++ Web/src/App.js
@@ -0,0 +1,45 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Context } from './components/Contexto';
+import { RegisterContext } from './components/ContextoRegister';
+import { useContext, useEffect} from 'react';
+import Home from './components/Home';
+import Movie from './components/Movie';
+import Profile from './components/Profile';
+import Category from './components/Category';
+import NavBar from './components/NavBar';
+import InvalidRoute from './components/InvalidRoute';
+import ApiError from './components/ApiError';
+
+function App() {
+
+  const isLogged = !!localStorage.getItem("token");
+  const [contextState, setContextState] = useContext(Context);
+  const [registerState, setRegisterState] = useContext(RegisterContext);
+
+ 
+ 
+    
+
+  return (
+    <BrowserRouter>
+      <div className='wholePage' style={{  
+        pointerEvents: !isLogged && (contextState.bool || registerState) ? 'none' : 'auto',
+      }}>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route path="/movie/:id" element={<Movie/>} />
+        <Route path="/movie/*" element={<Home/>} />
+        <Route path="/profile/:id" element={<Profile/>} />
+        <Route path="/category/:id" element={<Category/>} />
+        <Route path="*" element={<InvalidRoute/>} />
+        <Route path="/invalidRoute" element={<InvalidRoute/>} />
+        <Route path="/apiError" element={<ApiError/>} />
+      </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
